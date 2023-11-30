b2e796bbc4a2b7912962494354bf4132a59aa71a
ibrouwer-exe <i.brouwer@uleth.ca>
2023-11-30 16:44:56 -0700

    fixed class stuff

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -1,4 +1,4 @@
 // Wait for HTML document to get ready
 window.addEventListener('load', function() { // NOT `DOMContentLoaded`
     // Do something abt html document
-    var draggable = new PlainDraggable(document.getElementsByClassName('tat1')[0], { // `PlainDraggable` is a class
+    var draggable = new PlainDraggable(document.getElementsByClassName('tat1')[0],
