b1a57be4438a0873f7cc7ac3aab5fee2109e745f
ibrouwer-exe <i.brouwer@uleth.ca>
2023-12-05 16:34:02 -0700

    LITCHRALLY FIXED IT DO NOT CHANGE VAR

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -1,4 +1,10 @@
 // Wait for HTML document to get ready
 window.addEventListener('load', function() { // NOT `DOMContentLoaded`
-    // Do something abt html document
-    var draggable = new PlainDraggable(document.getElementsByClassName('tat1')[0],
+    // Do something about HTML document
+    var draggable = new PlainDraggable(document.getElementsByClassName('target')[0,1]);
+    console.log(draggable);
+    draggable.containment = {left: 0, top: 0, width: '100%', height: '100%'};
+  });
+
+  
+
