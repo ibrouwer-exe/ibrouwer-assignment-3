151789b7df3da1a1e0c9833bfb0ab66e5be539c7
ibrouwer-exe <i.brouwer@uleth.ca>
2023-12-05 20:51:32 -0700

    actually fixed jscript

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -1,9 +1,22 @@
 // Wait for HTML document to get ready
 window.addEventListener('load', function() { // NOT `DOMContentLoaded`
-    // Do something about HTML document
-    var draggable = new PlainDraggable(document.getElementsByClassName('target')[0,1]);
-    console.log(draggable);
-    draggable.containment = {left: 0, top: 0, width: '100%', height: '100%'};
+    
+    var tattoo1 = new PlainDraggable(document.getElementById('tat1'));
+      tattoo1.containment = {left: 0, top: 0, width: '100%', height: '100%'};
+    var tattoo2 = new PlainDraggable(document.getElementById('tat2'));
+      tattoo2.containment = {left: 0, top: 0, width: '100%', height: '100%'};
+    var tattoo3 = new PlainDraggable(document.getElementById('tat3'));
+      tattoo3.containment = {left: 0, top: 0, width: '100%', height: '100%'};
+    var tattoo4 = new PlainDraggable(document.getElementById('tat4'));
+      tattoo4.containment = {left: 0, top: 0, width: '100%', height: '100%'};
+    var tattoo5 = new PlainDraggable(document.getElementById('tat5'));
+      tattoo5.containment = {left: 0, top: 0, width: '100%', height: '100%'};
+    var tattoo6 = new PlainDraggable(document.getElementById('tat6'));
+      tattoo6.containment = {left: 0, top: 0, width: '100%', height: '100%'};
+    var tattoo7 = new PlainDraggable(document.getElementById('tat7'));
+      tattoo7.containment = {left: 0, top: 0, width: '100%', height: '100%'};
+    var tattoo8 = new PlainDraggable(document.getElementById('tat8'));
+      tattoo8.containment = {left: 0, top: 0, width: '100%', height: '100%'};
   });
 
   
