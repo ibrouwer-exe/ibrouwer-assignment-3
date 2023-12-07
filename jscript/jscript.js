9b16be67830fd2a026397f39c02d2cad69b20b05
ibrouwer-exe <i.brouwer@uleth.ca>
2023-12-07 00:14:26 -0700

    changes

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -1,20 +1,18 @@
 // Wait for HTML document to get ready
 window.addEventListener('load', function() { // NOT `DOMContentLoaded`
 
-    var tattoo1 = new PlainDraggable(document.getElementById('tat1'));
+    var tattoo1 = new PlainDraggable(document.getElementById('Anchor'));
       tattoo1.containment = {left: 0, top: 0, width: '100%', height: '100%'};
-    var tattoo2 = new PlainDraggable(document.getElementById('tat2'));
+    var tattoo2 = new PlainDraggable(document.getElementById('Rope'));
       tattoo2.containment = {left: 0, top: 0, width: '100%', height: '100%'};
-    var tattoo3 = new PlainDraggable(document.getElementById('tat3'));
+    var tattoo3 = new PlainDraggable(document.getElementById('Star'));
       tattoo3.containment = {left: 0, top: 0, width: '100%', height: '100%'};
-    var tattoo4 = new PlainDraggable(document.getElementById('tat4'));
+    var tattoo4 = new PlainDraggable(document.getElementById('Wheel'));
       tattoo4.containment = {left: 0, top: 0, width: '100%', height: '100%'};
-    var tattoo5 = new PlainDraggable(document.getElementById('tat5'));
-      tattoo5.containment = {left: 0, top: 0, width: '100%', height: '100%'};
-    var tattoo6 = new PlainDraggable(document.getElementById('tat6'));
+  
+    var tattoo6 = new PlainDraggable(document.getElementById('twoflower'));
       tattoo6.containment = {left: 0, top: 0, width: '100%', height: '100%'};
-    var tattoo7 = new PlainDraggable(document.getElementById('tat7'));
-      tattoo7.containment = {left: 0, top: 0, width: '100%', height: '100%'};
+  
     var tattoo8 = new PlainDraggable(document.getElementById('tat8'));
       tattoo8.containment = {left: 0, top: 0, width: '100%', height: '100%'};
   });
